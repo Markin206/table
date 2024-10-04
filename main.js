4902638ae9989a707b6a1699bd4e222fb29c8d2a
Lorant Marton <lorant.marton@BOLYAI.LOCAL>
2024-10-04 13:01:43 +0200

    form fejlesztese 3 fazisig

--- main.js
+++ main.js
@@ -111,3 +111,19 @@ for(const person of array){
         }
     )  
 }
+
+const form = document.getElementById('form');
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const lastname = document.getElementById('lastname');
+    const firstname1 = document.getElementById('firstname1')
+    const firstname2 = document.getElementById('firstname2')
+    const married = document.getElementById('married');
+    const pet = document.getElementById('pet');
+
+    const lastnameValue = lastname.value;
+    const firstname1Value = firstname1.value;
+    const firstname2Value = firstname2.value;
+    const marriedChecked = married.checked;
+    const petValue = pet.value;
+})
